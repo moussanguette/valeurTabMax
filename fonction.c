58bc7306489848fc13fc40ee1297a552db6cc45b
Moussa Nguette <mossanguette@gmail.com>
2020-11-12 12:21:57 +0000

    ajout de commnetaire dans le programme

--- fonction.c
+++ fonction.c
@@ -1,31 +1,33 @@
 #include<stdio.h>
-    
-float tab[3];
+   
+float tab[3];// declaration globale d'un tableau
+// fonction permettant de saisir dans un tableau
 void remplirTableau(int i){
-    printf("donner la valeur N°%d\n",i+1);
-    scanf("%f",&tab[i]);
+        printf("donner la valeur N°%d\n",i+1);
+        int test=scanf("%f",&tab[i]);
 }
-
+// fonction permettant d'afficher les elements du tableau
 void afficherTableau(){
     printf("les valeur du tableau sont :\n");
     for ( int i = 0; i <=3; i++)
         printf("%f\t",tab[i]);
     printf("\n");
     }
-
+// fonction permettant de determiner la valeur max du tableau
 float determinerMaximun(){
-    float valeurMax=tab[0];
-    int indice=0;
-    for (int i = 0; i <= 3; i++)
+    int indice=0;//initialisation de la variable par la premiere indice du tableau
+    float valeurMax=tab[0];// initialisation de la variable par le premier element du tableau
+    for (int i = 0; i <= 3; i++)//parcourir le tableau
     {
-        for (int j = 1;  j<=3; j++)
+        for (int j = 1;  j<=3; j++)// parcourir le tableau tout en commencant par la deuxieme valeur du tab
         {
-            if (tab[j]>tab[i] && tab[j]>valeurMax)
-                valeurMax=tab[j];
+            if (tab[j]>tab[i] && tab[j]>valeurMax)// comparer les valeurs du tableau
+                valeurMax=tab[j];// affecter le max dans la variable valeurMax
         }
     }
     return valeurMax;
 }
+//fonction permettant de determiner l'indice de la plus grande valeur
 int indiceMaximun(){
     float valeurMax=tab[0];
     int indice=0;
